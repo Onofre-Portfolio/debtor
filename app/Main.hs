b9a9d45ada1986c9fa576338afe501021354c2ec
OnofreTZK <taraujonofre@gmail.com>
2025-03-13 13:04:41 -0300

    Parsing args

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,19 @@
 module Main where
 
+import System.Environment (getArgs)
+
+tryParseCmd :: [String] -> Maybe String
+tryParseCmd [cmd] = Just cmd
+tryParseCmd _ = Nothing
+
 main :: IO ()
 main =
   do
-    command <- getLine
-    case command of 
-      "add" -> putStrLn "Add to the debt"
-      "pay" -> putStrLn "Good job you have paid a part."
-      _ -> putStrLn "Invalid command! [print help]"
+    args <- getArgs
+    case tryParseCmd args of
+      Just cmd ->
+        case cmd of
+          "add" -> putStrLn "Add to the debt"
+          "pay" -> putStrLn "Good job you have paid a part."
+          _ -> putStrLn "Invalid command! [print help]"
+      Nothing -> putStrLn "Please, provide a command [print help]"
