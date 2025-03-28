5c0c1103d2d86bcbd19d4b609c3d419905c8037a
OnofreTZK <taraujonofre@gmail.com>
2025-03-27 21:52:34 -0300

    Deployed but still need fixes

--- flake.nix
+++ flake.nix
@@ -1,26 +1,38 @@
 {
   description = "Debtor Shell";
 
- inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs = {
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: 
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system: let
-      pkgs = import nixpkgs { inherit system; };
-      ghc = "ghc984";
-      haskellPackages = pkgs.haskell.packages.${ghc};
-    in
-    {
-      devShell = haskellPackages.shellFor {
-        packages = p: [];  # Add project packages if needed
-        buildInputs = with haskellPackages; [
-          cabal-install
-          hlint
-          haskell-language-server
-        ];
-        withHoogle = true;
-      };
-    });
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        ghc = "ghc984";
+        haskellPackages = pkgs.haskell.packages.${ghc};
+      in
+      {
+        devShells.default = haskellPackages.shellFor {
+          packages = p: [];  # Add your project packages here
+          buildInputs = with haskellPackages; [
+            cabal-install
+            hlint
+            haskell-language-server
+          ];
+          withHoogle = true;
+        };
+
+        devShells.deploy = pkgs.mkShell {
+          buildInputs = with haskellPackages; [
+            cabal-install
+          ];
+
+          shellHook = ''
+            cabal build
+            cabal install --installdir=/Users/tiagoonofre/Learning/BIN --overwrite-policy=always
+            exit
+          '';
+        };
+      });
 }
